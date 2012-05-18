a59acac3a8bf4c8e00e16d3ec3292131b74a2e01
Luca Fabbri <luca@keul.it>
2012-05-18 16:12:27 +0200

    moved to rst README format

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='redturtle.smartlink',
       version=version,
       description=("An advanced Link content type for Plone, "
                    "with image field, customizable content icon and internal link feature"),
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
