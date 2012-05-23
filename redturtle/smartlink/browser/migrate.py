07cfc7d9c2ba0efde145e3cc72c8c5c07d4d1004
Luca Fabbri <luca@keul.it>
2012-05-23 15:14:29 +0200

    Undoing useless patch
    This reverts commit d5dc4e53bd5d8c8927187771bcf88b12e8db5e46.

--- redturtle/smartlink/browser/migrate.py
+++ redturtle/smartlink/browser/migrate.py
@@ -6,7 +6,7 @@ try: # >= 4.1
     from five.formlib import formbase
 except ImportError: # < 4.1
     from Products.Five.formlib import formbase
-from redturtle.smartlink.migrator import migrateSmartLink
+from redturtle.smartlink.migrator import migrateSmartLink, migrateLinkToSmartLink
 from Products.statusmessages.interfaces import IStatusMessage
 
 from redturtle.smartlink import smartlinkMessageFactory as _
@@ -42,3 +42,25 @@ class MigrateBlobs(formbase.PageForm):
     @form.action(_(u'Cancel'))
     def actionCancel(self, action, data):
         return self.request.response.redirect(self.context.absolute_url())
+
+
+class MigrateToSmartLink(formbase.PageForm):
+    form_fields = form.FormFields(IMigrateBlobsSchema)
+    label = _(u'Migrate ATLink to Smart Link')
+    description = _(u'Migrate basic Plone ATLink to Smart Link')
+
+    @form.action(_(u'Migrate'))
+    def actionMigrate(self, action, data):
+        output = migrateLinkToSmartLink(self.context)
+        cnt = 0
+        for l in output:
+            cnt+=1
+            IStatusMessage(self.request).addStatusMessage(l, type='info')
+        IStatusMessage(self.request).addStatusMessage(_('update_count_message',
+                                                        default="${count} elements updated", mapping={'count': cnt}),
+                                                      type='info')
+        return self.request.response.redirect(self.context.absolute_url() + '/@@smartlink-config')
+
+    @form.action(_(u'Cancel'))
+    def actionCancel(self, action, data):
+        return self.request.response.redirect(self.context.absolute_url() + '/@@smartlink-config')
